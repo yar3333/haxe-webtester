b15f7aa25f16ed6c112e80722809c49db52b8a3d
Yaroslav <Yaroslav@YHOST>
2012-06-02 17:35:05 +0400

    + init

---
+++ tests/neko/TestsA.hx
@@ -0,0 +1,14 @@
+package;
+
+class TestsA extends haxe.unit.TestCase
+{
+    public function testBasic()
+    {
+		//print("\n");
+		//print("  std time: " + Date.now().getTime() + "\t" + Date.now() + "\n");
+		//print("micro time: " + MicroTime.now().getTime() + "\n");
+		//print("micro time: " + microtime.Date.now().getTime() + "\t" + microtime.Date.now() + "\n");
+        
+		this.assertTrue(true);
+    }
+}
